0e0670ceca0f5d8444b4f70118e257583fb159d2
PLUM <you@example.com>
2019-08-26 15:37:46 +0200

    Abprallen von Ball gemacht

--- main.c
+++ main.c
@@ -8,13 +8,30 @@
 #include "glcd/fonts/Liberation_Sans15x21_Numbers.h"
 #include "glcd/fonts/font5x7.h"
 #include <avr/pgmspace.h>
+#include <util/delay.h>
 #define F_CPU 16000000UL  // 1 MHz
 #define	LINKS	!(PIND &(1<<PD6))
 #define RECHTS	!(PIND &(1<<PD5))
+#define UP 1	
+#define DOWN 0
+#define LEFT 1
+#define RIGHT 0
+
+ISR (TIMER1_COMPA_vect)
+{
+	
+}
+
 int main(void)
 {
-	char Richtung;
-	char hoehe;
+	char Balken_x;
+	char Balken_y;
+	char Balkenlaenge;
+	char ball_y;
+	char start;
+	char ball_x;
+	char ball_vert;
+	char ball_horiz;
 	
 	DDRB|=(1<<PB2);
 	DDRC|=(1<<PC3);
@@ -23,8 +40,29 @@ int main(void)
 	
 	DDRD&=~((1<<PD5)|(1<<PD6)|(1<<PD2));
 	PORTD|=((1<<PD5)|(1<<PD6)|(1<<PD2));
-	Richtung=35;
-	hoehe=0;
+	
+	//Timer 1 Configuration
+	OCR1A = 0x009C;	//OCR1A = 0x3D08;==1sec
+	
+    TCCR1B |= (1 << WGM12);
+    // Mode 4, CTC on OCR1A
+
+    TIMSK1 |= (1 << OCIE1A);
+    //Set interrupt on compare match
+
+    TCCR1B |= (1 << CS12) | (1 << CS10);
+    // set prescaler to 1024 and start the timer
+
+	
+	Balken_x=35;
+	Balkenlaenge=15;
+	ball_y=30;
+	ball_x=50;
+	Balken_y=42;
+	start=0;
+	ball_vert=DOWN;
+	ball_horiz=RIGHT;
+	
 	
 	sei();
 
@@ -33,9 +71,6 @@ int main(void)
 	glcd_write();
 	glcd_tiny_set_font(Font5x7 ,5,7,32,127);
 	glcd_clear_buffer();
-
-	glcd_draw_rect(00,00,84,47,1);
-	glcd_draw_rect(48,42,15,3,1);
 	
 	glcd_write();	
 		
@@ -44,48 +79,57 @@ int main(void)
 		{
 			if(LINKS)
 			{
-				glcd_clear();
-				
-				Richtung--;
-				glcd_draw_rect(Richtung,42,15,3,1);
-				glcd_write();
-				
-				if(Richtung <= 1)
+				Balken_x=Balken_x--;
+				if(Balken_x < 1)
 				{
-					Richtung = 2;
+					Balken_x = 2;
 				}
 			}
 			if(RECHTS)
 			{
-				glcd_clear();
-				Richtung++;
-				glcd_draw_rect(Richtung,42,15,3,1);
-				glcd_write();
-				if(Richtung >= 68)
+				Balken_x=Balken_x++;
+				if(Balken_x > 68)
 				{
-					Richtung = 67;
+					Balken_x = 67;
 				}
 			}
-			/*if(!(PIND&(1<<PD2)))
-			{												//später hinzufügen (Höhe)
-				while(!(PIND&(1<<PD2)))
-				glcd_clear();
-				hoehe--;
-				glcd_draw_rect(Richtung,hoehe,15,3,1);
-				glcd_write();
-				if(hoehe<30)
-				{
-					hoehe=29;
-				}
-				
-				
-			}*/
-				
 			
+			if(ball_vert==DOWN)
+			{
+				ball_y++;
+			}else ball_y--;
+			
+			/*if(start==0)
+			{
+				ball_y++;
 				
+				if (ball_y+2>42)
+				{
+					start++;
+				}
+			}*/
+			
+			
+			if((ball_y+2>=Balken_y)&&(ball_x>=Balken_x)&&(ball_x<=(Balken_x+Balkenlaenge))&&(ball_vert==DOWN)) //Abprall ab Balken
+			{
+				ball_vert=UP;
+			}else if((ball_vert==UP)&&(ball_y<=2))  //Abprall an Oberem Rand
+					{
+						ball_vert=DOWN;
+					}
+				
+				
+			glcd_clear();
+			
+			glcd_draw_rect(Balken_x,Balken_y,Balkenlaenge,3,1);
+			
+			glcd_fill_circle(ball_x,ball_y,2,1);	
+			
 			glcd_draw_rect(00,00,84,47,1);
+			
 			glcd_write();
 			
+			_delay_ms(10);
 		}
 				
 	
