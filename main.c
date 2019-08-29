a8a055d256714c2ad87457db55955aef9ba1cc24
PLUM <you@example.com>
2019-08-29 16:08:04 +0200

    Blöcke hinzugef¨ügt und zum verschwinden gebracht

--- main.c
+++ main.c
@@ -16,12 +16,15 @@
 #define DOWN 0
 #define LEFT 0
 #define RIGHT 1
+#define delete goto
 
 
-unsigned char ms10=0;
-unsigned char ms100=0;
+//====================================================================
+
+
+unsigned char ms10=0; 			
+unsigned char ms100=0;			//variabeln für Timer erstellen
 unsigned char Bewegung=0;
-
 
 ISR (TIMER1_COMPA_vect)
 {
@@ -30,7 +33,7 @@ ISR (TIMER1_COMPA_vect)
 	{
 		Bewegung++;
 		ms10=0;
-		ms100++;
+		ms100++;				//Timer, zeit bestimmen
 	}
 	if(ms100==3)
 	{
@@ -39,8 +42,18 @@ ISR (TIMER1_COMPA_vect)
 	}
 }
 
+
+//|||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||| START MAIN ||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||
+
+
 int main(void)
 {
+
+
+
+//====================================================================Variabeln erstellen
+
+
 	char Balken_x;
 	char Balken_y;
 	char Balkenlaenge;
@@ -49,24 +62,38 @@ int main(void)
 	char ball_vert;
 	char ball_horiz=10;
 	
+	unsigned char color1=1;
+	unsigned char color2=1;
+	unsigned char color3=1;
+	unsigned char color4=1;
+	unsigned char color5=1;
 	
+	unsigned char xBOX1=4;
+	unsigned char xBOX2=20;
+	unsigned char xBOX3=36;
+	unsigned char xBOX4=52;
+	unsigned char xBOX5=68;
 	
-/*	
-	char buffer[10];
-	char buffer1[10];
-	char buffer2[10];
-	*/
-	
-	
-	
-	DDRB|=(1<<PB2);
-	DDRC|=(1<<PC3);
-	PORTC&=~(1<<PC3);
+	unsigned char yBOX1=2;
+	unsigned char yBOX2=2;
+	unsigned char yBOX3=2;
+	unsigned char yBOX4=2;
+	unsigned char yBOX5=2;
+
+
+
+//====================================================================Ports für Taster als Input definieren	
 
 	
-	DDRD&=~((1<<PD5)|(1<<PD6)|(1<<PD2));
-	PORTD|=((1<<PD5)|(1<<PD6)|(1<<PD2));
-//====================================================================
+	DDRD|=(1<<PB2);						//Hintergrundbeleuchtung definieren
+	PORTB|=(1<<PB2);						//Hintergrundbeleuchtung ein
+	
+	DDRD&=~((1<<PD5)|(1<<PD6)|(1<<PD2));	//Taster als Output definieren
+	PORTD|=((1<<PD5)|(1<<PD6)|(1<<PD2));	//Pullup aktivieren
+
+	
+//====================================================================Timer einstellungen
+
 	
 	//Timer 1 Configuration
 	OCR1A = 1270;	//OCR1A = 0x3D08;==1sec
@@ -79,53 +106,49 @@ int main(void)
 
     TCCR1B |= (1 << CS11)|(1 << CS10);
     // set prescaler to 64 and start the timer
-//====================================================================
+
+
+//====================================================================Variabeln Deklarieren
+
 	
 	Balken_x=35;
 	Balkenlaenge=15;
 	ball_y=12;
-	ball_x=42;
+	ball_x=42;					//Balken und Ball Variabeln Deklarieren
 	Balken_y=42;
-	ball_vert=DOWN;
+	ball_vert=DOWN;							
 	ball_horiz=10;
 	
 	
-	glcd_fill_circle(ball_x,ball_y,2,1);
+	
+	glcd_fill_circle(ball_x,ball_y,2,1); //Startposition Ball Zeichnen
 
 	sei();
 	glcd_init();
-	glcd_clear();
+	glcd_clear();						//Display einstellungen
 	glcd_write();
 	glcd_tiny_set_font(Font5x7 ,5,7,32,127);
-	
-	
-										/*while (1){
-										glcd_clear_buffer();
-										sei(); //enable global interrupt
-										
-										sprintf(buffer2,"%d",Milisec);
-										glcd_tiny_draw_string(30,1,buffer2);
-										
-										
-										sprintf(buffer,"%d",Sekunde);
-										glcd_tiny_draw_string(30,2,buffer);
-										
-										sprintf(buffer1,"%d",Minute);
-										glcd_tiny_draw_string(30,3,buffer1);
-										glcd_write();*/
 
-											
-		
+
+//====================================================================
+
+
+///////////////////////////////////////////////////////////////// START While //////////////////////////////////////////////////////////////////
+	
+	
 		while(1)
 		{
-//======================================================================			
+			
+		
+	
+		//----------------------------------------------------------------	
 			if(LINKS)
 			{
 				Balken_x=Balken_x--;
 				if(Balken_x < 1)
 				{
 					Balken_x = 2;
-				}							//balkenbewegungen nach Links und rechts
+				}						//balkenbewegungen nach Links und rechts
 			}
 			if(RECHTS)
 			{
@@ -135,48 +158,60 @@ int main(void)
 					Balken_x = 67;
 				}
 			}
+		//----------------------------------------------------------------------
 
-//======================================================================
+		
+
+//<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<< Ball Bewegungen >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+
 
 			if((ball_vert==DOWN)&&(Bewegung==1)) 
 			{
-				ball_y++;			//Ball Bewegung nach Unten
+				ball_y++;							//Ball Bewegung nach Unten
 			}						
 
-//--------------------------------------------------------------
+		//----------------------------------------------------------------------
 
 			if((ball_vert==UP)&&(Bewegung==1)) 
 			{
-				ball_y--;						//Ball Bewegung nach Oben
+				ball_y--;							//Ball Bewegung nach Oben
 			}
 
-//----------------------------------------------------------------------
+		//----------------------------------------------------------------------
 
 			if((ball_horiz==LEFT)&&(Bewegung==1)) 
 			{
-				ball_x--;						//Ball Bewegung nach Links
+				ball_x--;							//Ball Bewegung nach Links
 			}
 
-//----------------------------------------------------------------------
+		//----------------------------------------------------------------------
 
 			if((ball_horiz==RIGHT)&&(Bewegung==1)) 
 			{
-				ball_x++;						//Ball Bewegung nach Rechts
+				ball_x++;							//Ball Bewegung nach Rechts
 			}
 
-//--------------------------------------------------------			
+		//---------------------------------------------------------------------		
 
-			if((ball_y+2>=Balken_y)&&(ball_x>=Balken_x+5)&&(ball_x<=(Balken_x+10))&&(ball_vert==DOWN)) //Abprall nach oben
+
+
+//<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<< Ball abprallen >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+
+
+
+			if((ball_y+2>=Balken_y)&&(ball_x>=Balken_x+5)&&(ball_x<=(Balken_x+10))&&(ball_vert==DOWN)&&(ball_y+2<=Balken_y+1))	//Abprall nach oben
 			{
 				ball_vert=UP;
-			}else if((ball_vert==UP)&&(ball_y<=2))  //Abprall an Oberem Rand
+			}else if((ball_vert==UP)&&(ball_y<=2))  														//Abprall an Oberem Rand
 					{
 						ball_vert=DOWN;
 					}
 
-//--------------------------------------------------------			
 
-			if((ball_y+2>=Balken_y)&&(ball_x<=(Balken_x+5))&&(ball_x>=Balken_x)&&(ball_vert==DOWN)) //Winkel nach links
+		//---------------------------------Richtungswechsel an Balken------------------------------------		
+
+
+			if((ball_y+2>=Balken_y)&&(ball_x<=(Balken_x+5))&&(ball_x>=Balken_x)&&(ball_vert==DOWN)&&(ball_y+2<=Balken_y+1)) //Winkel nach links
 			{
 				ball_horiz=LEFT;
 				ball_vert=UP;
@@ -185,7 +220,7 @@ int main(void)
 						ball_horiz=RIGHT;
 					}else 
 					
-			if((ball_x>=(Balken_x+10))&&(ball_x<=(Balken_x+15))&&(ball_y>=Balken_y)&&(ball_vert==DOWN)) //Winkel nach rechts
+			if((ball_x>=(Balken_x+10))&&(ball_x<=(Balken_x+15))&&(ball_y+2>=Balken_y)&&(ball_vert==DOWN)&&(ball_y+2<=Balken_y+1)) //Winkel nach rechts
 			{
 				ball_horiz=RIGHT;
 				ball_vert=UP;
@@ -193,23 +228,57 @@ int main(void)
 					{
 						ball_horiz=LEFT;
 					}
-//---------------------------------------------------------
+		//***********************************ENDE Bewegungselemente***********************************
 			
-				
-				
-			glcd_clear_buffer();											//alles wird gezeichned
-			
-			glcd_draw_rect(Balken_x,Balken_y,Balkenlaenge,3,1);
-			
-			glcd_fill_circle(ball_x,ball_y,2,1);	
-			
-			glcd_draw_rect(00,00,84,47,1);
-			
-			glcd_write();
-			
-			
-		}
-				
+//__________________________________________________________________________________________________________
+											/*Blöcke generieren und dessen Verhalten*/
+			 
+		//------------------------------------------------------------------------------------
+			if((ball_y+2<=(yBOX1+4)&&(ball_x>=xBOX1)&&(ball_x<=(xBOX1+12))&&(color1==1))) 
+			{
+				color1=0;																	//Verschwinden von box1
+				ball_vert=DOWN;
+			}
+		//------------------------------------------------------------------------------------
+			if((ball_y+2<=(yBOX2+4)&&(ball_x>=xBOX2)&&(ball_x<=(xBOX2+12))&&(color1==1))) 
+			{
+				color2=0;																	//Verschwinden von box2
+				ball_vert=DOWN;
+			}
+		//------------------------------------------------------------------------------------
+			if((ball_y+2<=(yBOX3+4)&&(ball_x>=xBOX3)&&(ball_x<=(xBOX3+12))&&(color3==1))) 
+			{
+				color3=0;																	//Verschwinden von box3
+				ball_vert=DOWN;
+			}
+	 				
+	//-----------------------------------------------------------		
+		glcd_clear_buffer();
 		
+		glcd_fill_rect(xBOX1,yBOX1,12,4,color1);	
+		glcd_fill_rect(xBOX2,yBOX2,12,4,color2);	
+		glcd_fill_rect(xBOX3,yBOX3,12,4,color3);	
+		glcd_fill_rect(xBOX4,yBOX4,12,4,color4);	
+		glcd_fill_rect(xBOX5,yBOX5,12,4,color5);	
+		
+		
+			
+		glcd_draw_rect(Balken_x,Balken_y,Balkenlaenge,3,1);
+			
+		glcd_fill_circle(ball_x,ball_y,2,1);					//alles wird gezeichned
+			
+		glcd_draw_rect(00,00,84,47,1);
+			
+		glcd_write();
+	//-----------------------------------------------------------		
 	
-}
+
+	
+////////////////////////////////////////////////////////////////// ENDE While //////////////////////////////////////////////////////////////////
+		
+		}//End while		
+		
+		
+		
+//|||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||| START MAIN ||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||
+} //End main
